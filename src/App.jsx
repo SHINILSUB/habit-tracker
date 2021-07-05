9a4f631da880c53e6238cd168dca7cb68ba8d25d
SHINILSUB <deathsizes@hotmail.com>
2021-07-05 17:47:17 +0900

    first

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+import habit from './components/habit';
+import React, { Component } from 'react';
+
+
+function App() {
+  return (
+    <>
+    <span className="habit-name">Reading</span>
+    <button>
+    <i class="fas fa-plus"></i>
+    </button>
+    </>
+    
+  );
+}
+
+export default App;
