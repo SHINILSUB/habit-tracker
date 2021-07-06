7e922238a05e6855950884b96118ce8e70f27ba3
SHINILSUB <deathsizes@hotmail.com>
2021-07-06 14:31:00 +0900

    habits

--- src/components/habit.jsx
+++ src/components/habit.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-class habit extends Component {
+class Habit extends Component {
     render() {
         state = {
             count = 0,
@@ -12,10 +12,12 @@ class habit extends Component {
             count = this.state.count = - 1
             this.setState({ count: count < 0 ? 0 : count});
         }
+        console.log(this.props.habit);
+        const {name, count} =this.props.habit;
         return (
 
         <li className="habit">
-        <span className="habit-name">Reading</span>
+        <span className="habit-name">{name}</span>
         <span className="habit-count">{this.state.count}</span>
         <button className="habit-button habit-increase" onClick={this.handelInclement}>
         <i className="fas fa-plus"></i>
@@ -32,4 +34,4 @@ class habit extends Component {
     }
 }
 
-export default habit;
+export default Habit;
