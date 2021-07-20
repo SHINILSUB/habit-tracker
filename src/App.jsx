a569fb3f9cc83d5f441071852d3f3b8dee1ac844
SHINILSUB <deathsizes@hotmail.com>
2021-07-20 13:36:59 +0900

    propsMove

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,45 @@
+import React, { Component } from "react";
+import "./app.css";
+import Habits from "./components/habits";
 
-import './App.css';
-import Habits from './components/Habits';
-import React from 'react';
-
-
-function App() {
-  return (
-    <Habits/>    
-    
-  );
+class App extends Component {
+  state = {
+    habits: [
+      { id: 1, name: "reading", count: 0 },
+      { id: 2, name: "runnung", count: 0 },
+      { id: 3, name: "coding", count: 0 },
+    ],
+  };
+  handelInclement = (habit) => {
+    console.log(`handleInclement ${habit.name}`);
+    const habits = [...this.state.habits];
+    const index = habits.indexOf(habit);
+    habits[index].count++;
+    this.setState({ habits });
+  };
+  handelDecrement = (habit) => {
+    console.log(`handleDecrement ${habit.name}`);
+    const habits = [...this.state.habits];
+    const index = habits.indexOf(habit);
+    const count = habits[index].count - 1;
+    habits[index].count = count < 0 ? 0 : count;
+    this.setState({ habits });
+  };
+  handleDelete = (habit) => {
+    console.log(`handleDelete ${habit.id}`);
+    const habits = this.state.habits.filter((item) => item.id !== habit.id);
+    this.setState({ habits });
+  };
+  render() {
+    return (
+      <Habits
+        habits={this.state.habits}
+        onIncrement={this.handelInclement}
+        onDecrement={this.handelDecrement}
+        onDlete={this.handleDelete}
+      />
+    );
+  }
 }
 
 export default App;
--- src/components/Habits.jsx
+++ src/components/Habits.jsx
@@ -2,39 +2,21 @@ import React, { Component } from 'react';
 import Habit from './Habit';
 
 class Habits extends Component {
-    state = {
-        habits: [
-            { id:1, name: 'reading', count: 0},
-            { id:2, name: 'runnung', count: 0},
-            { id:3, name: 'coding', count: 0},
-        ],
-    };
+    
     handelInclement = habit => {
-        console.log(`handleInclement ${habit.name}`)
-        const habits = [...this.state.habits];
-        const index = habits.indexOf(habit);
-        habits[index].count++;
-        this.setState({habits});
-    }
-    handelDecrement = habit => {
-        console.log(`handleDecrement ${habit.name}`)
-        const habits = [...this.state.habits];
-        const index = habits.indexOf(habit);
-        const count = habits[index].count - 1;
-        habits[index].count = count < 0 ? 0 : count;
-        this.setState({habits});
-    }
-    handleDelete = habit => {
-        console.log(`handleDelete ${habit.id}`);
-        const habits = this.state.habits.filter(item => item.id !== habit.id);
-        this.setState({habits});
-        
+        this.props.onInclement(habit) };
 
-    }
+    handelDecrement = habit => {
+        this.props.onDecrement(habit)
+    };
+    handleDelete = habit => {
+        this.props.onDelete(habit)
+
+    };
     render() {
         return (
             <ul>
-                {this.state.habits.map(habit =>(
+                {this.props.habits.map(habit =>(
                     <Habit key={habit.id} 
                     habit={habit} 
                     onInclement={this.handelInclement}
